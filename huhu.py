0815a5f564a1fe14fbe5f1284ea308399a131f7d
IkbalMift <ikbalmiftahudin1@gmail.com>
2024-11-07 15:11:24 +0700

    penambahan file huhu.py

---
+++ huhu.py
@@ -0,0 +1,8 @@
+a = 5
+b = 7
+
+# Menghitung hasil perkalian
+hasil = a * b
+
+# Menampilkan hasil
+print(f"Hasil dari {a} x {b} = {hasil}")
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ print("Hello World")
 
 #Penambahan
 print("Hello Ikbal Ganteng Banget ")
+print("Hello Ikbal Cantik Banget")
